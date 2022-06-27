53b5d14b16625f66842b27d7f5bdc4eddb8a94ba
Abayomi <abiyo4reel@gmail.com>
2022-06-27 15:39:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
+
+class App extends Component {
+
+  state = {
+    userInput: ''
+  }
+
+  inputChangedHandler = (event) =>{
+    this.setState({
+      userInput: event.target.value
+    }) 
+  }
+deleteCharHandler = (index) =>{
+  const kaka = this.state.userInput.split('');
+  kaka.splice(index, 1)
+  const update = kaka.join('')
+  this.setState({
+    userInput: update
+  })
+}
+
+ 
+  render(){
+
+    const charList = this.state.userInput.split('').map((ch, index) =>{
+      return <Char character = {ch} key = {index} clicking ={()=>this.deleteCharHandler(index)}></Char>
+    })
+  
+    
+    return (
+    <div className='App'>
+      <input type='text' onChange = {this.inputChangedHandler}></input>
+      <h1>{this.state.userInput.length}</h1>
+      <Validation inputLength = {this.state.userInput.length}></Validation>
+      {charList}
+    </div>
+  );
+
+}
+
+}
+
+export default App;
